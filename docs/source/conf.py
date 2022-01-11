bb6b6c53fc6a167b447512942b063b2f71e05758
simioniandrea <andreasimioni5@gmail.com>
2022-01-11 17:42:27 +0100

    fffffff

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'sphinxcontrib.openapi'
+    'sphinx.ext.sphinxcontrib-openapi'
 ]
 
 intersphinx_mapping = {
